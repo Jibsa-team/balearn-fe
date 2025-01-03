f76cf2100f703e12373287fe71c56bd48418fac8
JaeIn1 <97165077+JaeIn1@users.noreply.github.com>
2025-01-03 14:56:41 +0900

    feat : 루트 레이아웃 수정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,3 @@
-import Header from "@/components/header/header";
 import "./globals.css";
 import Sidebar from "@/components/sidebar/sidebar";
 
@@ -12,12 +11,7 @@ export default function RootLayout({
       <body>
         <div className="flex">
           <Sidebar />
-          <div className="flex flex-col w-full bg-sidebarBg">
-            <Header />
-            <main className="p-10 flex flex-1 justify-center items-center">
-              {children}
-            </main>
-          </div>
+          {children}
         </div>
       </body>
     </html>
