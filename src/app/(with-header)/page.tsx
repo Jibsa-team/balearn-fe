2ade55dcc9763c868a933efbadb925737d5b34d0
JaeIn1 <97165077+JaeIn1@users.noreply.github.com>
2025-01-07 08:28:13 +0900

    refactor : css 수정

--- src/app/(with-header)/page.tsx
+++ src/app/(with-header)/page.tsx
@@ -5,7 +5,7 @@ import WeeklyStudy from "./weeklyStudy";
 
 export default function Home() {
   return (
-    <div className="w-[100%]  bg-white shadow-md p-10 ">
+    <div className="w-[95%] p-[30px] bg-white shadow-md overflow-y-scroll">
       <Notify />
       <Daliy />
       <Member />
