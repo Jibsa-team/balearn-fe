63d9bcdeffd833426ee5d5659b186db23a774fd7
JaeIn1 <97165077+JaeIn1@users.noreply.github.com>
2025-01-07 08:27:31 +0900

    refactor : 페이지 레이아웃 수정

--- src/app/(with-header)/layout.tsx
+++ src/app/(with-header)/layout.tsx
@@ -3,9 +3,14 @@ import { ReactNode } from "react";
 
 export default function Layout({ children }: { children: ReactNode }) {
   return (
-    <main className="flex flex-col w-full h-screen bg-sidebarBg">
+    <main className="flex flex-col w-full bg-sidebarBg">
       <Header />
-      <div className="flex justify-center items-center overflow-y-auto">
+      <div
+        className="flex justify-center px-10 py-0 overflow-y-auto"
+        style={{
+          height: "calc(100vh - 103px)",
+        }}
+      >
         {children}
       </div>
     </main>
