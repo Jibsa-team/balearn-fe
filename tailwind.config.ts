1060d3dfe1dcaf376a9bbd7dc9aefae25d34527a
JaeIn1 <97165077+JaeIn1@users.noreply.github.com>
2025-01-07 08:30:05 +0900

    feat : 비활성화 색상 추가

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -16,6 +16,7 @@ const config: Config = {
         activeColor: "#ADB8CC",
         unActiveColor: "#ADB8CC",
         logoColor: "#C9D439",
+        disabledColor: "rgba(0, 0, 0, 0.2)",
         notifyColor: "#C7C7C71A",
       },
     },
