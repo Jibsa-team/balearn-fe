0ae1861f5e3a77b102bff5578f627d7d6d9d7a55
JaeIn1 <97165077+JaeIn1@users.noreply.github.com>
2025-01-06 14:55:15 +0900

    refactor : 채팅 페이지 레이아웃 수정

--- src/app/chat/layout.tsx
+++ src/app/chat/layout.tsx
@@ -3,7 +3,7 @@ import { ReactNode } from "react";
 export default function Layout({ children }: { children: ReactNode }) {
   return (
     <main className="flex flex-col w-full bg-sidebarBg">
-      <div className="p-10 flex flex-1 justify-center items-center">
+      <div className="pt-[0px] flex flex-1 justify-center items-center">
         {children}
       </div>
     </main>
