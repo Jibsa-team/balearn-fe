9b6033c7ef2fa4be75edc29862560b7d70ddaea6
JaeIn1 <97165077+JaeIn1@users.noreply.github.com>
2025-01-10 15:38:23 +0900

    feat : 캘린더 사이드바 반응형으로 수정

--- src/components/calendar/sideModal.tsx
+++ src/components/calendar/sideModal.tsx
@@ -1,30 +1,73 @@
-import React from "react";
+// CanlendarSideModal.tsx
+"use client";
+
+import React, { useEffect, useState } from "react";
 import { motion } from "framer-motion";
-import { calendarDay } from "@/utils/calendar";
 import CalendarGoal from "./goal";
+import CalendarDate from "./date";
 
 function CanlendarSideModal({
   isOpen,
-  currentDate,
+  selectDate,
+  view,
+  handleAddEvent,
 }: {
   isOpen: boolean;
-  currentDate: Date;
+  selectDate: Date;
+  view: string;
+  handleAddEvent: (
+    selectedGoal: string,
+    startDate: Date,
+    endDate: Date,
+    startTime: string,
+    endTime: string
+  ) => void;
 }) {
+  const [selectedGoal, setSelectedGoal] = useState<string>("");
+  const [startDate, setStartDate] = useState<Date | null>(selectDate);
+  const [endDate, setEndDate] = useState<Date | null>(selectDate);
+  const [startTime, setStartTime] = useState<string>("");
+  const [endTime, setEndTime] = useState<string>("");
+
+  useEffect(() => {
+    setStartDate(selectDate);
+    setEndDate(selectDate);
+  }, [selectDate]);
+
+  const handlerUpdateEvent = () => {
+    handleAddEvent(selectedGoal, startDate!, endDate!, startTime, endTime);
+  };
+
   return (
     isOpen && (
       <motion.div
-        className="w-[500px] bg-white shadow-lg rounded-lg p-4 md:z-40"
-        initial={{ x: 300, opacity: 0 }} // 초기 위치와 불투명도 설정
-        animate={{ x: 0, opacity: 1 }} // 열렸을 때의 애니메이션
-        exit={{ x: 300, opacity: 0 }} // 닫힐 때 애니메이션
-        transition={{ type: "spring", stiffness: 300, damping: 30 }} // 애니메이션 속도와 탄성 조정
+        className={`bg-white shadow-lg rounded-lg p-4 w-[300px] z-40 absolute right-0 top-0 h-[100%]
+          lg:w-[500px] lg:z-10 lg:static`}
+        initial={{ x: 300, opacity: 0 }}
+        animate={{ x: 0, opacity: 1 }}
+        exit={{ x: 300, opacity: 0 }}
+        transition={{ type: "spring", stiffness: 300, damping: 30 }}
       >
         <div>
-          <div className="text-xl flex justify-center items-center">
-            {calendarDay(currentDate)}
-          </div>
-          <h1 className="text-xl font-semibold">목표</h1>
-          <CalendarGoal />
+          <CalendarGoal
+            setSelectedGoal={setSelectedGoal}
+            selectedGoal={selectedGoal}
+          />
+          <CalendarDate
+            view={view}
+            setStartDate={setStartDate}
+            setEndDate={setEndDate}
+            setStartTime={setStartTime}
+            setEndTime={setEndTime}
+            startDate={startDate}
+            endDate={endDate}
+          />
+          <button
+            onClick={handlerUpdateEvent}
+            className="w-full bg-logoColor text-white mt-[30px] py-[5px] rounded-lg"
+          >
+            추가하기
+          </button>
         </div>
       </motion.div>
     )
