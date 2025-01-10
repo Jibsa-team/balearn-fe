28952357f540f9068e1fda373610dbda65d8cde9
JaeIn1 <97165077+JaeIn1@users.noreply.github.com>
2025-01-10 15:38:08 +0900

    feat : 캘린더 사이드바 달력 추가

---
+++ src/components/calendar/select.calendar.tsx
@@ -0,0 +1,36 @@
+"use client";
+
+import React, { useRef } from "react";
+import DatePicker from "react-date-picker";
+import "react-date-picker/dist/DatePicker.css";
+import "react-calendar/dist/Calendar.css";
+import { FaRegCalendarMinus } from "react-icons/fa";
+
+function SelectCalendar({
+  onDateChange,
+  selectDate,
+}: {
+  onDateChange: (date: Date | null) => void;
+  selectDate: Date | null;
+}) {
+  const calendarRef = useRef<HTMLDivElement>(null);
+
+  const handleDateChange = (date: Date | null) => {
+    onDateChange(date); // 선택된 날짜를 부모 컴포넌트로 전달
+  };
+
+  return (
+    <div ref={calendarRef} className="relative">
+      <DatePicker
+        onChange={handleDateChange}
+        value={selectDate}
+        calendarIcon={<FaRegCalendarMinus className="text-logoColor" />}
+        clearIcon={null}
+        format="yyyy-MM-dd"
+        className="w-[200px] border-[1px] h-[41px] border-gray-300 rounded-md text-[0.9rem]"
+      />
+    </div>
+  );
+}
+
+export default SelectCalendar;
