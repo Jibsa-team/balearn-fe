519795a1ab8d61f50031702d2eecfd07f889dfc4
JaeIn1 <97165077+JaeIn1@users.noreply.github.com>
2025-01-14 15:23:01 +0900

    feat : 로그아웃 모달 추가

---
+++ src/components/modal/profileModal.tsx
@@ -0,0 +1,48 @@
+"use client";
+
+import { motion, AnimatePresence } from "framer-motion";
+import { MdOutlineLogout } from "react-icons/md";
+
+interface ModalProps {
+  isOpen: boolean;
+  onClose: () => void;
+  onLogout: () => void;
+}
+
+const modalVariants = {
+  hidden: { opacity: 0, y: -10 },
+  visible: { opacity: 1, y: 0 },
+  exit: { opacity: 0, y: -10 },
+};
+
+const ProfileModal = ({ isOpen, onClose, onLogout }: ModalProps) => {
+  return (
+    <AnimatePresence>
+      {isOpen && (
+        <motion.div
+          className="absolute right-0 mt-2 w-[150px] bg-white border rounded-md shadow-lg z-10"
+          initial="hidden"
+          animate="visible"
+          exit="exit"
+          variants={modalVariants}
+          transition={{ duration: 0.2 }}
+        >
+          <button
+            onClick={onLogout}
+            className="block w-full text-left px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
+          >
+            <span>로그아웃</span>
+          </button>
+          <button
+            onClick={onClose}
+            className="block w-full text-left px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
+          >
+            취소
+          </button>
+        </motion.div>
+      )}
+    </AnimatePresence>
+  );
+};
+
+export default ProfileModal;
