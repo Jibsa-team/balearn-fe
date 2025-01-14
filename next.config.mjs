37d983ea366009c2c5e06349184cc22f1e6ab705
JaeIn1 <97165077+JaeIn1@users.noreply.github.com>
2025-01-14 15:18:37 +0900

    feat : oAuth 로그인 이미지 경로 추가

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["example.com", "developers.kakao.com"],
+    domains: [
+      "example.com",
+      "developers.kakao.com",
+      "lh3.googleusercontent.com",
+      "k.kakaocdn.net",
+    ],
   },
 };
 
