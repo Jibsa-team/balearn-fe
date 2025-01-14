5d4903cd88c3803d289f7e817f82364fa5d69f7b
JaeIn1 <97165077+JaeIn1@users.noreply.github.com>
2025-01-14 15:24:22 +0900

    feat : 로그인 관련 타입 지정

---
+++ src/types/user/user.ts
@@ -0,0 +1,10 @@
+export interface User {
+  createdAt: string;
+  email: string;
+  id: number;
+  modifiedAt: number;
+  name: string;
+  phoneNumber: null;
+  profileImageUrl: string;
+  provider: string;
+}
