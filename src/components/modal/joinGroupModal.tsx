f7eb6927851304ee09f918e78cee03c309dec0fe
JaeIn1 <97165077+JaeIn1@users.noreply.github.com>
2025-01-16 18:15:37 +0900

    feat : 모임 , 프로필 모달 구현 완료

---
+++ src/components/modal/joinGroupModal.tsx
@@ -0,0 +1,84 @@
+"use client";
+
+import { AnimatePresence, motion } from "framer-motion";
+import { useState } from "react";
+import { IoMdClose } from "react-icons/io";
+
+interface JoinGroupModalProps {
+  isOpen: boolean;
+  onClose: () => void;
+  onJoin: (code: string) => void;
+}
+
+const JoinGroupModal: React.FC<JoinGroupModalProps> = ({
+  isOpen,
+  onClose,
+  onJoin,
+}) => {
+  const [code, setCode] = useState("");
+
+  const modalVariants = {
+    hidden: { opacity: 0, scale: 0.8 },
+    visible: { opacity: 1, scale: 1, transition: { duration: 0 } },
+  };
+
+  const handleCodeChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setCode(e.target.value);
+  };
+
+  const handleJoinClick = () => {
+    onJoin(code);
+    setCode("");
+  };
+
+  return (
+    <AnimatePresence>
+      {isOpen && (
+        <motion.div
+          className="fixed inset-0 flex items-center justify-center  bg-opacity-50 z-50"
+          initial="hidden"
+          animate="visible"
+          exit="hidden"
+          variants={modalVariants}
+        >
+          <motion.div
+            className="bg-white rounded-xl p-6 md:w-[400px] w-[300px] shadow-2xl"
+            initial={{ scale: 0.8 }}
+            animate={{ scale: 1 }}
+            exit={{ scale: 0.8, opacity: 0 }}
+            transition={{ duration: 0.2 }}
+          >
+            <header className="flex justify-between items-center mb-4">
+              <h2 className="text-[1.1rem] font-semibold text-center">
+                전달받은 코드를 입력해주세요
+              </h2>
+              <IoMdClose
+                onClick={onClose}
+                className="text-2xl cursor-pointer"
+              />
+            </header>
+
+            <input
+              type="text"
+              value={code}
+              onChange={handleCodeChange}
+              placeholder="코드를 입력하세요"
+              className="w-full border-[1px] border-gray-300 rounded-lg p-2 mb-4"
+            />
+
+            <div className="flex justify-center mt-6">
+              <button
+                onClick={handleJoinClick}
+                className="w-full bg-logoColor text-white px-4 py-2 rounded-lg"
+              >
+                가입하기
+              </button>
+            </div>
+          </motion.div>
+        </motion.div>
+      )}
+    </AnimatePresence>
+  );
+};
+
+export default JoinGroupModal;
--- src/components/sidebar/sidebar.tsx
+++ src/components/sidebar/sidebar.tsx
@@ -1,20 +1,67 @@
+"use client";
+
 import { motion } from "framer-motion";
 import Image from "next/image";
 import SidebarItem from "./sidebar.item";
 import useAuthStore from "@/store/useAuthStore";
+import Link from "next/link";
+import { Dispatch, SetStateAction, useEffect, useState } from "react";
+import JoinGroupModal from "@/components/modal/joinGroupModal"; // Import the modal component
+import { useParams } from "next/navigation";
+import { fetchWithAuth } from "@/app/lib/fetchWithAuth"; // Assuming you have a fetchWithAuth function
+import { DashboardType } from "@/types/dashboard/dashboard";
 
 interface SidebarProps {
   isSidebarOpen: boolean;
+  setIsSidebarOpen: Dispatch<SetStateAction<boolean>>;
 }
 
-function Sidebar({ isSidebarOpen }: SidebarProps) {
+function Sidebar({ isSidebarOpen, setIsSidebarOpen }: SidebarProps) {
+  const [isJoinModalOpen, setIsJoinModalOpen] = useState(false);
+  const [teamInfo, setTeamInfo] = useState<{
+    name: string;
+    imageUrl: string;
+  } | null>(null);
+
+  const { id } = useParams();
+
+  useEffect(() => {
+    const fetchTeamInfo = async () => {
+      if (id) {
+        const response = await fetchWithAuth(
+          `${process.env.NEXT_PUBLIC_API_URL}/api/team/${id}`
+        );
+        if (response.ok) {
+          const data: DashboardType = await response.json();
+          console.log(data);
+          setTeamInfo({
+            name: data.result.team.name,
+            imageUrl: data.result.team.imgUrl,
+          });
+        }
+      }
+    };
+
+    fetchTeamInfo();
+  }, [id]);
+
+  const handleOpenModal = () => {
+    setIsSidebarOpen(false);
+    setIsJoinModalOpen(true);
+  };
+  const handleCloseModal = () => setIsJoinModalOpen(false);
+  const handleJoinGroup = () => {
+    console.log("모임에 가입했습니다!");
+    setIsJoinModalOpen(false);
+  };
+
   return (
     <>
       <div
         className="hidden lg:block w-[300px] bg-white shadow-xl"
         style={{ height: "calc(100vh - 70px)" }}
       >
-        <Content />
+        <Content handleOpenModal={handleOpenModal} teamInfo={teamInfo} />
       </div>
 
       <motion.div
@@ -27,47 +74,71 @@ function Sidebar({ isSidebarOpen }: SidebarProps) {
           top: "70px",
         }}
       >
-        <Content />
+        <Content handleOpenModal={handleOpenModal} teamInfo={teamInfo} />
       </motion.div>
+
+      <JoinGroupModal
+        isOpen={isJoinModalOpen}
+        onClose={handleCloseModal}
+        onJoin={handleJoinGroup}
+      />
     </>
   );
 }
 
-function Content() {
+function Content({
+  handleOpenModal,
+  teamInfo,
+}: {
+  handleOpenModal: () => void;
+  teamInfo: { name: string; imageUrl: string } | null;
+}) {
   const user = useAuthStore((state) => state.user);
-  return (
-    <div className="h-full p-[20px] flex flex-col items-center bg-white lg:bg-sidebarBg">
-      {/* 모임명 */}
-      <section className="w-full flex flex-col items-start mb-[20px]">
-        <div className="w-full flex items-center justify-between cursor-pointer">
-          <div className="w-full flex items-center justify-center">
-            <div className="rounded-full p-[5px] border-[2.5px] w-[40px] h-[40px] overflow-hidden mr-[10px] relative">
-              {user?.profileImageUrl ? (
-                <Image
-                  src={user?.profileImageUrl}
-                  alt="user img"
-                  layout="fill"
-                  className="rounded-full object-cover"
-                />
-              ) : (
-                <div className="w-[30px] h-[30px] bg-gray-300 rounded-full"></div>
-              )}
-            </div>
-            <span className="text-[1.1rem] mt-[3px]">OurMeeting</span>
-          </div>
-        </div>
-      </section>
-      <div className="w-full border-[1px] border-gray-[rgba(0,0,0,0.05)] mt-[10px]"></div>
 
-      <main className="w-[80%] flex flex-col items-center">
-        <SidebarItem />
-      </main>
-      <button
-        className="mt-[10px] px-[15px] py-[8px] text-sm text-white bg-logoColor rounded-md hover:bg-blue-600 transition"
-        onClick={() => alert("모임 생성하기 클릭!")}
-      >
-        모임 생성하기
-      </button>
+  console.log(teamInfo);
+
+  return (
+    <div className="h-full p-[20px] flex flex-col items-center justify-between bg-white lg:bg-sidebarBg">
+      <div>
+        <section className="w-full flex flex-col items-start mb-[20px]">
+          <div className="w-full flex items-center justify-between cursor-pointer">
+            <div className="w-full flex items-center justify-center">
+              <div className="rounded-full p-[5px] border-[2.5px] w-[40px] h-[40px] overflow-hidden mr-[10px] relative">
+                {teamInfo?.imageUrl ? (
+                  <Image
+                    src={teamInfo?.imageUrl}
+                    alt="team img"
+                    layout="fill"
+                    className="rounded-full object-cover"
+                  />
+                ) : (
+                  <div className="w-[30px] h-[30px] bg-gray-300 rounded-full"></div>
+                )}
+              </div>
+              <span className="text-[1.1rem] mt-[3px]">{teamInfo?.name}</span>
+            </div>
+          </div>
+        </section>
+        <div className="w-full border-[1px] border-gray-[rgba(0,0,0,0.05)] mt-[10px]"></div>
+
+        <main className="w-[80%] flex flex-col items-center">
+          <SidebarItem />
+        </main>
+      </div>
+
+      <div className="w-full flex flex-col items-center">
+        <Link href={"/group"}>
+          <button className="mt-[10px] px-[40px] py-[8px] text-sm text-white bg-logoColor rounded-lg">
+            모임 생성하기
+          </button>
+        </Link>
+        <button
+          onClick={handleOpenModal}
+          className="mt-[10px] px-[40px] py-[8px] text-sm text-white bg-logoColor rounded-lg cursor-pointer"
+        >
+          모임 가입하기
+        </button>
+      </div>
     </div>
   );
 }
