908cf675760410eea01bbecace8722184de0a561
JaeIn1 <97165077+JaeIn1@users.noreply.github.com>
2025-01-16 18:14:47 +0900

    feat : useGroupStore 추가 완료

---
+++ src/store/useGroupStore.ts
@@ -0,0 +1,22 @@
+import { User } from "@/types/user/user";
+import { create } from "zustand";
+import { persist } from "zustand/middleware";
+
+interface GroupStore {
+  groupId: number;
+  setGroupId: (id: number) => void;
+}
+
+const useGroupStore = create<GroupStore>()(
+  persist(
+    (set) => ({
+      groupId: 0,
+      setGroupId: (id: number) => set({ groupId: id }),
+    }),
+    {
+      name: "group-storage",
+    }
+  )
+);
+
+export default useGroupStore;
