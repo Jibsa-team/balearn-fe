7b19a26b9c829c9137871e73c36774c2e734a717
JaeIn1 <97165077+JaeIn1@users.noreply.github.com>
2025-01-21 08:14:20 +0900

    feat : 공지 관리 리스트 empty 컴포넌트 추가

--- src/app/(with-header)/setting/[id]/notify/notify.main.tsx
+++ src/app/(with-header)/setting/[id]/notify/notify.main.tsx
@@ -10,6 +10,8 @@ import { useQuery } from "@tanstack/react-query";
 import { useParams } from "next/navigation";
 import TeamNotifySkeleton from "@/components/skeleton/teamNotifySkelton";
 import { NotificationDto } from "@/types/notify/teamNotifyDto";
+import { NotifyDate } from "@/app/lib/date";
+import Empty from "@/components/empty/empty";
 
 const getAllNotify = async (
   teamId: number,
@@ -44,53 +46,67 @@ function NotifyMain() {
     setActiveModalId(null);
   };
 
-  if (isError) return <div>오류가 발생했습니다.</div>;
+  if (isLoading) {
+    return (
+      <>
+        {Array.from({ length: 10 }).map((_, index) => (
+          <div key={index}>
+            <TeamNotifySkeleton />
+          </div>
+        ))}
+      </>
+    );
+  }
+
+  if (isError) {
+    return <div>오류가 발생했습니다.</div>;
+  }
+
+  if (!data || !data.result.content || data.result.content.length === 0) {
+    return <Empty message={"등록된 공지가 없습니다"} />;
+  }
 
   return (
     <>
-      {!isLoading
-        ? data &&
-          data.result.content.map((notice) => (
+      {data.result.content.map((notice) => (
+        <div
+          key={notice.id}
+          className="flex justify-between md:items-center items-start border-b-[1px] border-gray-200 mb-[20px] pb-[10px]"
+        >
+          <div className="w-full flex flex-col md:flex-row items-start md:items-center">
+            <div className="flex items-center gap-4 mb-4 md:mb-0 md:mr-[20px]">
+              <div className="w-[30px] h-[30px] relative cursor-pointer mr-[5px]">
+                <Image
+                  src={user?.profileImageUrl as string}
+                  alt="user img"
+                  layout="fill"
+                  className="rounded-full object-cover"
+                />
+              </div>
+              <div className="mt-[13px] ml-[5px]">
+                <div className="text-[1rem]">{user!.name}</div>
+                <div className="text-[0.8rem] text-gray-500">
+                  {NotifyDate(notice.createdAt)}
+                </div>
+              </div>
+            </div>
+            <div className="w-full">{notice.title}</div>
+          </div>
+          <div className="md:mt-[0px] mt-[15px]">
             <div
-              key={notice.id}
-              className="flex justify-between items-center border-b-[1px] border-gray-200 mb-[20px] pb-[10px]"
+              className="cursor-pointer relative"
+              onClick={() => handleToggleModal(notice.id)}
             >
-              <div className="flex items-center">
-                <div className="flex items-center mr-[30px]">
-                  <div className="w-[30px] h-[30px] relative cursor-pointer mr-[5px]">
-                    <Image
-                      src={user?.profileImageUrl as string}
-                      alt="user img"
-                      layout="fill"
-                      className="rounded-full object-cover"
-                    />
-                  </div>
-                  <div>
-                    <span>{user!.name}</span>
-                  </div>
-                </div>
-                <div>{notice.title}</div>
-              </div>
-              <div>
-                <div
-                  className="cursor-pointer relative"
-                  onClick={() => handleToggleModal(notice.id)}
-                >
-                  <BsThreeDots />
-                  <NotifyModal
-                    isOpen={activeModalId === notice.id}
-                    onClose={handleCloseModal}
-                    id={activeModalId}
-                  />
-                </div>
-              </div>
+              <BsThreeDots />
+              <NotifyModal
+                isOpen={activeModalId === notice.id}
+                onClose={handleCloseModal}
+                id={activeModalId}
+              />
             </div>
-          ))
-        : Array.from({ length: 10 }).map((_, index) => (
-            <div key={index}>
-              <TeamNotifySkeleton />
-            </div>
-          ))}
+          </div>
+        </div>
+      ))}
     </>
   );
 }
