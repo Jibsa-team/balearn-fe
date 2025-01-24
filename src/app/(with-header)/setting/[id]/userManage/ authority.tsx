2c5475b6a3b48520cdb530e7814035a788e4e710
JaeIn1 <97165077+JaeIn1@users.noreply.github.com>
2025-01-24 13:51:52 +0900

    feat : 회원 권한 변경 기능 구현완료

--- src/app/(with-header)/setting/[id]/userManage/ authority.tsx
+++ src/app/(with-header)/setting/[id]/userManage/ authority.tsx
@@ -1,19 +1,67 @@
 "use client";
 
+import { fetchWithAuth } from "@/app/lib/fetchWithAuth";
+import { useToast } from "@/hooks/use-toast";
+import { DashboardType, TeamUser } from "@/types/dashboard/dashboard";
+import { useQuery, useQueryClient } from "@tanstack/react-query";
 import Image from "next/image";
+import { useParams } from "next/navigation";
 import { useState } from "react";
 import { MdKeyboardArrowDown } from "react-icons/md";
 
+const fetchMemberData = async (
+  groupId: string | string[]
+): Promise<DashboardType> => {
+  const response = await fetchWithAuth(
+    `${process.env.NEXT_PUBLIC_API_URL}/api/team/${groupId}`
+  );
+  if (!response.ok) {
+    throw new Error("Failed to fetch member details");
+  }
+  return response.json();
+};
+
+const getRoleLabel = (role: string) => {
+  switch (role) {
+    case "관리자":
+      return "leader";
+    case "팀원":
+      return "member";
+  }
+};
+
 function UserAuthority() {
-  const [selectedUser, setSelectedUser] = useState<string>("");
+  const [selectedUser, setSelectedUser] = useState<TeamUser>();
   const [selectedRole, setSelectedRole] = useState<string>("");
   const [isUserOpen, setIsUserOpen] = useState<boolean>(false);
   const [isRoleOpen, setIsRoleOpen] = useState<boolean>(false);
+  const [updateLoading, setUpdateLoading] = useState(false);
 
-  const users = ["황민우", "장경우", "임대영", "이승준"];
-  const role = ["관리자", "멤버"];
+  const { id } = useParams();
+  const queryClient = useQueryClient();
+  const { toast } = useToast();
 
-  const handleSelectUser = (user: string) => {
+  const { data, isLoading, isError } = useQuery({
+    queryKey: ["dashboardData", id],
+    queryFn: () => fetchMemberData(id),
+  });
+
+  const { data: userData } = useQuery({
+    queryKey: ["teamUserMe", id],
+    queryFn: () =>
+      fetchWithAuth(
+        `${process.env.NEXT_PUBLIC_API_URL}/api/team/${id}/me`
+      ).then((res) => res.json()),
+  });
+
+  if (isLoading) return <div>로딩 중...</div>;
+  if (isError) return <div>오류가 발생했습니다.</div>;
+
+  const teamUsers = (data?.result?.teamUser || []).filter(
+    (user) => user.userId !== userData?.result?.userId
+  );
+
+  const handleSelectUser = (user: TeamUser) => {
     setSelectedUser(user);
     setIsUserOpen(false);
   };
@@ -22,9 +70,58 @@ function UserAuthority() {
     setSelectedRole(role);
     setIsRoleOpen(false);
   };
+
+  const handleAuthority = async () => {
+    if (!selectedUser || !selectedRole) {
+      toast({
+        title: "권한 변경 실패",
+        description: "회원과 권한을 모두 선택해주세요.",
+        variant: "destructive",
+      });
+      return;
+    }
+
+    try {
+      setUpdateLoading(true);
+      const response = await fetchWithAuth(
+        `${process.env.NEXT_PUBLIC_API_URL}/api/team/${id}/user/${
+          selectedUser.id
+        }?role=${getRoleLabel(selectedRole)}`,
+        {
+          method: "PUT",
+        }
+      );
+
+      const result = await response.json();
+
+      if (result.responseCode === "SUCCESS") {
+        toast({
+          title: "권한 변경 성공",
+          description: "회원 권한이 성공적으로 변경되었습니다.",
+          variant: "default",
+        });
+
+        queryClient.invalidateQueries({
+          queryKey: ["dashboardData", id],
+        });
+      } else {
+        throw new Error(result.message);
+      }
+    } catch (err) {
+      toast({
+        title: "권한 변경 실패",
+        description:
+          err instanceof Error ? err.message : "오류가 발생했습니다.",
+        variant: "destructive",
+      });
+    } finally {
+      setUpdateLoading(false);
+    }
+  };
+
   return (
     <div>
-      <div className="w-[50%]">
+      <div className="md:w-[50%] w-full">
         <h2 className="text-[1.1rem] font-semibold mb-[20px]">권한</h2>
         <div className="mb-[30px]">
           <span>회원 선택</span>
@@ -37,35 +134,37 @@ function UserAuthority() {
               <div className="flex items-center">
                 {selectedUser && (
                   <Image
-                    src={"/Avatar.png"}
+                    src={selectedUser.imgUrl || "/Avatar.png"}
                     alt="user image"
                     width={30}
                     height={30}
                     className="rounded-full mr-[10px]"
                   />
                 )}
-                <span>{selectedUser || "선택하세요"}</span>
+                <span>
+                  {(selectedUser && selectedUser.nickname) || "선택하세요"}
+                </span>
               </div>
               <MdKeyboardArrowDown className="text-gray-400" />
             </div>
 
             {isUserOpen && (
               <div className="absolute top-[100%] left-0 w-full bg-white border-[1px] border-gray-300 mt-1 rounded-md shadow-lg z-10">
-                {users.map((user) => (
+                {teamUsers.map((user) => (
                   <div
-                    key={user}
+                    key={user.id}
                     className="cursor-pointer p-[10px] hover:bg-[rgba(0,0,0,0.05)]"
                     onClick={() => handleSelectUser(user)}
                   >
                     <div className="flex items-center">
                       <Image
-                        src={"/Avatar.png"}
+                        src={user.imgUrl || "/Avatar.png"}
                         alt="user image"
                         width={30}
                         height={30}
                         className="rounded-full mr-[10px]"
                       />
-                      <span>{user}</span>
+                      <span>{user.nickname}</span>
                     </div>
                   </div>
                 ))}
@@ -90,9 +189,9 @@ function UserAuthority() {
 
             {isRoleOpen && (
               <div className="absolute top-[100%] left-0 w-full bg-white border-[1px] border-gray-300 mt-1 rounded-md shadow-lg z-10">
-                {role.map((role, i) => (
+                {["관리자", "팀원"].map((role) => (
                   <div
-                    key={i}
+                    key={role}
                     className="cursor-pointer p-[10px] hover:bg-[rgba(0,0,0,0.05)]"
                     onClick={() => handleSelectRole(role)}
                   >
@@ -106,8 +205,16 @@ function UserAuthority() {
           </div>
         </div>
         <div className="flex justify-between items-center">
-          <button className="w-[45%] bg-[#2DA44E] text-white py-[5px] rounded-lg cursor-pointer">
-            변경
+          <button
+            onClick={handleAuthority}
+            disabled={updateLoading}
+            className={`w-[45%] ${
+              updateLoading
+                ? "bg-[#2DA44E]/50 text-white cursor-not-allowed"
+                : "bg-[#2DA44E] text-white cursor-pointer"
+            } py-[5px] rounded-lg`}
+          >
+            {updateLoading ? "변경중.." : "변경"}
           </button>
           <button className="w-[45%] bg-[#B52324] text-white py-[5px] rounded-lg cursor-pointer">
             방출
--- src/app/(with-header)/setting/[id]/userManage/invite.tsx
+++ src/app/(with-header)/setting/[id]/userManage/invite.tsx
@@ -23,11 +23,12 @@ function UserInvite() {
       <div className="mt-[50px]">
         <h2 className="text-[1.1rem] font-semibold mb-[20px]">초대</h2>
         <div className="mb-[20px]">회원 초대</div>
-        <div className="w-[50%] flex justify-between items-center">
+        <div className="md:w-[50%] w-full flex justify-between items-center">
           <input
             type="text"
             value={code}
             placeholder="code.."
+            readOnly
             className="flex-1 border-[1px] border-[rgba(0,0,0,0.1)] rounded-2xl px-[10px] py-[5px]"
           />
           <button
