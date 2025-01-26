b1ba6ef7412e5b9a1c331952b06c319aafca8ff6
JaeIn1 <97165077+JaeIn1@users.noreply.github.com>
2025-01-26 12:58:23 +0900

    feat : 일정 추가 스켈레톤 추가

--- src/components/skeleton/calendar.event.tsx
+++ src/components/skeleton/calendar.event.tsx
@@ -2,12 +2,31 @@ import { Skeleton } from "../ui/skeleton";
 
 export default function CalendarEventSkeleton() {
   return (
-    <div
-      className={`bg-white  rounded-lg w-[300px] z-20 absolute right-0 top-0 h-[100%]
-        md:w-[440px] lg:z-10 lg:static`}
-    >
-      <Skeleton className="h-4 w-full" />
-      <Skeleton className="h-4 w-full" />
+    <div className="bg-white shadow-lg rounded-lg p-4 w-[300px] z-20 absolute right-0 top-0 h-[100%] md:w-[500px] lg:z-10 lg:static">
+      <div className="mb-[20px]">
+        <Skeleton className="h-[24px] w-1/2 mb-4" />
+        <div className="mb-[20px]">
+          <Skeleton className="h-[20px] w-1/3 mb-2" />
+          <Skeleton className="h-[40px] w-full" />
+        </div>
+
+        <div className="mb-[20px]">
+          <Skeleton className="h-[20px] w-1/3 mb-2" />
+          <Skeleton className="h-[40px] w-full" />
+          <Skeleton className="h-[40px] w-full mt-2" />
+        </div>
+
+        <div className="mb-[20px]">
+          <Skeleton className="h-[20px] w-1/3 mb-2" />
+          <div className="flex justify-between">
+            <Skeleton className="h-[40px] w-[45%]" />
+            <Skeleton className="h-[40px] w-[45%]" />
+          </div>
+        </div>
+
+        <Skeleton className="h-[40px] w-full mt-[30px]" />
+        <Skeleton className="h-[40px] w-full mt-[10px]" />
+      </div>
     </div>
   );
 }
