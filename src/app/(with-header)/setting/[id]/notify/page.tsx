99100c2117ca361a9d15d1591458e5af858e7a71
JaeIn1 <97165077+JaeIn1@users.noreply.github.com>
2025-01-26 12:55:04 +0900

    refactor : 공지 페이지 csa 수정

--- src/app/(with-header)/setting/[id]/notify/page.tsx
+++ src/app/(with-header)/setting/[id]/notify/page.tsx
@@ -4,9 +4,11 @@ import NotifyMain from "./notify.main";
 
 function SettingNotify() {
   return (
-    <div className="w-[100%] h-[100%] md:p-[30px] p-[15px] bg-white">
+    <div className="w-[100%] h-[calc(100vh-100px)] md:p-[30px] p-[15px] bg-white">
       <NotifyHeader />
-      <NotifyMain />
+      <div className="h-[calc(100%-60px)] overflow-y-auto">
+        <NotifyMain />
+      </div>
     </div>
   );
 }
