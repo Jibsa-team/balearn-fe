585359440b95731b0fb3130671f201ed39ed764d
JaeIn1 <97165077+JaeIn1@users.noreply.github.com>
2025-01-28 17:22:16 +0900

    feat : 파일 리스트 페이지 구현

--- src/app/(with-header)/files/[id]/myFiles.tsx
+++ src/app/(with-header)/files/[id]/myFiles.tsx
@@ -1,111 +1,141 @@
-import { UploadedFile } from "@/types/file/files";
+"use client";
+
+import { FileData, FileDto } from "@/types/file/files";
 import React, { useState } from "react";
 import { RiDeleteBin6Line } from "react-icons/ri";
 import { BsFileEarmarkPdfFill } from "react-icons/bs";
-import useFileStore from "@/store/useFileStore";
 import { fileSize } from "@/utils/file";
 import DeleteFileModal from "@/components/modal/deleteFileModal";
 import Empty from "@/components/empty/empty";
+import { useParams } from "next/navigation";
+import { fetchWithAuth } from "@/app/lib/fetchWithAuth";
+import { useQuery } from "@tanstack/react-query";
+import FileListSkeleton from "@/components/skeleton/fileListSkelton";
+import useAuthStore from "@/store/useAuthStore";
+import { useToast } from "@/hooks/use-toast";
+import useFileStore from "@/store/useFileStore";
+import { FileIcon } from "@/app/lib/fileType";
+import Image from "next/image";
 
-function MyFiles({
-  files,
-  DeleteFile,
-}: {
-  files: UploadedFile[];
-  DeleteFile: (name: string) => void;
-}) {
-  const addFile = useFileStore((state) => state.addFile);
-
+function MyFiles() {
+  const { id } = useParams();
   const [isModalOpen, setIsModalOpen] = useState(false);
-  const [fileToDelete, setFileToDelete] = useState<string | null>(null);
+  const [fileDelete, setFileDelete] = useState<FileData | null>(null);
+  const teamUser = useAuthStore((state) => state.teamUser);
+  const addFile = useFileStore((state) => state.addFile);
+  const { toast } = useToast();
 
-  const openDeleteModal = (fileName: string) => {
-    setFileToDelete(fileName);
+  const { data, isLoading } = useQuery({
+    queryKey: ["files", id],
+    queryFn: async () => {
+      const response = await fetchWithAuth(
+        `${process.env.NEXT_PUBLIC_API_URL}/api/file/team/${id}`
+      );
+      if (!response.ok) {
+        throw new Error("파일 목록을 불러오는데 실패했습니다.");
+      }
+      const data: FileDto = await response.json();
+      return data.result;
+    },
+  });
+
+  const openDeleteModal = (file: FileData) => {
+    if (teamUser?.id !== file.createdBy.id) {
+      toast({
+        title: "파일 삭제 실패",
+        description: "삭제할 권한이 없습니다.",
+        variant: "destructive",
+      });
+      return;
+    }
+    console.log(file);
+    setFileDelete(file);
     setIsModalOpen(true);
   };
 
   const closeDeleteModal = () => {
     setIsModalOpen(false);
-    setFileToDelete(null);
+    setFileDelete(null);
   };
 
-  const confirmDelete = () => {
-    if (fileToDelete) {
-      DeleteFile(fileToDelete);
-    }
-    closeDeleteModal();
-  };
-
-  console.log(files);
+  if (isLoading) {
+    return <FileListSkeleton />;
+  }
 
   return (
     <div>
-      <div className="text-xl flex justify-start mb-[10px]">모든 파일</div>
       <div className="mt-[20px] w-full bg-white">
-        {files.length > 0 ? (
+        {data && data.length > 0 ? (
           <table className="table-auto w-full border-collapse border border-gray-200">
             <thead className="bg-gray-100">
               <tr>
-                <th className="border border-gray-200 px-4 py-2 text-left">
+                <th className="border border-gray-200 md:px-4 md:py-2 px-2 py-1 text-left md:text-[1rem] text-[0.8rem] w-1/4">
                   파일 이름
                 </th>
-                <th className="border border-gray-200 px-4 py-2 text-left">
+                <th className="border border-gray-200 md:px-4 md:py-2 px-2 py-1 text-left md:text-[1rem] text-[0.8rem]">
                   파일 크기
                 </th>
-                <th className="border border-gray-200 px-4 py-2 text-left">
+                <th className="border border-gray-200 md:px-4 md:py-2 px-2 py-1 text-left md:text-[1rem] text-[0.8rem]">
                   소유자
                 </th>
-                <th className="border border-gray-200 px-4 py-2 text-left">
+                <th className="border border-gray-200 md:px-4 md:py-2 px-2 py-1 text-left md:text-[1rem] text-[0.8rem]">
                   최근 변경일
                 </th>
-                <th className="border border-gray-200 px-4 py-2 text-left">
+                <th className="border border-gray-200 md:px-4 md:py-2 px-2 py-1 text-left md:text-[1rem] text-[0.8rem]">
                   삭제
                 </th>
               </tr>
             </thead>
             <tbody>
-              {files.map((file, index) => (
-                <tr key={index} className="hover:bg-gray-50">
-                  <td className="border border-gray-200 px-4 py-2">
-                    <a
-                      href={file.preview}
-                      target="_blank"
-                      rel="noopener noreferrer"
-                      className="text-blue-600 hover:underline flex items-center"
-                      onClick={() => addFile(file)}
-                    >
-                      <BsFileEarmarkPdfFill className="text-red-600 mr-[5px] text-xl" />
-                      {file.name}
-                    </a>
-                  </td>
-                  <td className="border border-gray-200 px-4 py-2">
-                    {fileSize(file.size)}
-                  </td>
-                  <td className="border border-gray-200 px-4 py-2">
-                    {file.owner}
-                  </td>
-                  <td className="border border-gray-200 px-4 py-2">
-                    {file.lastModified}
-                  </td>
-                  <td className="border border-gray-200 px-4 py-2 cursor-pointer">
-                    <RiDeleteBin6Line
-                      onClick={() => openDeleteModal(file.name)}
-                    />
-                  </td>
-                </tr>
-              ))}
+              {data &&
+                data.map((file) => (
+                  <tr key={file.id} className="hover:bg-gray-50">
+                    <td className="border border-gray-200 md:px-4 md:py-5 px-2 py-4 md:text-[1rem] text-[0.8rem]">
+                      <a
+                        href={file.fileUrl}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        className="text-blue-600 hover:underline flex items-center max-w-full"
+                        onClick={() => addFile(file, id as string)}
+                      >
+                        <FileIcon type={file.type} />
+                        <span className="truncate w-16 md:w-full">
+                          {file.name}
+                        </span>
+                      </a>
+                    </td>
+                    <td className="border border-gray-200 md:px-4 md:py-2 px-2 py-1 md:text-[1rem] text-[0.8rem]">
+                      {fileSize(file.size)}
+                    </td>
+                    <td className="border border-gray-200 md:px-4 md:py-2 px-2 py-1 md:text-[1rem] text-[0.8rem]">
+                      {/* <div className="md:block hidden relative w-[20px] h-[20px] flex-shrink-0 mr-[10px]">
+                        <Image
+                          src={file.createdBy.imgUrl || "/Avatar.png"}
+                          alt="파일 작성자"
+                          layout="fill"
+                          className="rounded-full object-cover"
+                        />
+                      </div> */}
+                      <span>{file.createdBy.nickname}</span>
+                    </td>
+                    <td className="border border-gray-200 md:px-4 md:py-2 px-2 py-1 md:text-[1rem] text-[0.8rem]">
+                      {new Date(file.modifiedAt).toLocaleDateString()}
+                    </td>
+                    <td className="border border-gray-200 md:px-4 md:py-2 px-2 py-1 cursor-pointer md:text-[1rem] text-[0.8rem]">
+                      <RiDeleteBin6Line onClick={() => openDeleteModal(file)} />
+                    </td>
+                  </tr>
+                ))}
             </tbody>
           </table>
         ) : (
           <Empty message={"등록된 파일이 없어요."} />
         )}
       </div>
-
       <DeleteFileModal
         isOpen={isModalOpen}
-        fileName={fileToDelete || ""}
+        fileDelete={fileDelete!}
         onClose={closeDeleteModal}
-        onConfirm={confirmDelete}
       />
     </div>
   );
