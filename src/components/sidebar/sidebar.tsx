c18421f908efe99bf26fe1484a32222c7e8d11bf
JaeIn1 <97165077+JaeIn1@users.noreply.github.com>
2025-02-05 17:38:28 +0900

    refactor : 사이드바 css 수정

--- src/components/sidebar/sidebar.tsx
+++ src/components/sidebar/sidebar.tsx
@@ -142,12 +142,12 @@ function Content({
         </main>
       </div>
 
-      <div className="w-full flex flex-col items-start pl-[15px]">
+      <div className="w-full flex flex-col items-start">
         <Link
           href={`/setting/${id}/groupCreate`}
-          className="cursor-pointer group"
+          className="cursor-pointer group w-full flex justify-center"
         >
-          <div className="w-full flex justify-evenly items-center mt-[10px] text-[1rem] text-[rgba(0,0,0,0.6)]">
+          <div className="w-full flex justify-center items-center mt-[10px] text-[1rem] text-[rgba(0,0,0,0.6)]">
             <HiPlusCircle className="text-[rgba(0,0,0,0.2)] group-hover:text-[rgba(0,0,0,0.4)] text-[1.6rem] mr-[10px]" />
             <span className="text-[1.1rem] text-[rgba(0,0,0,0.4)] group-hover:text-[rgba(0,0,0,0.6)]">
               모임 생성하기
@@ -156,7 +156,7 @@ function Content({
         </Link>
         <div
           onClick={handleOpenModal}
-          className="flex justify-evenly items-center mt-[10px] text-[1rem] text-[rgba(0,0,0,0.6)] cursor-pointer group"
+          className="flex justify-center items-center mt-[10px] text-[1rem] text-[rgba(0,0,0,0.6)] cursor-pointer group w-full"
         >
           <FaUserFriends className="text-[rgba(0,0,0,0.2)] group-hover:text-[rgba(0,0,0,0.4)] text-[1.6rem] mr-[10px]" />
           <span className="text-[1.1rem] text-[rgba(0,0,0,0.4)] group-hover:text-[rgba(0,0,0,0.6)]">
