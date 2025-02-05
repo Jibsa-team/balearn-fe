dbbb5ac7b9d0831543ce9a8aa52a1cf67039e523
JaeIn1 <97165077+JaeIn1@users.noreply.github.com>
2025-02-05 15:15:42 +0900

    refactor : 캘린더 사이드 바 반응형 수정

--- src/components/calendar/sideModal.event.tsx
+++ src/components/calendar/sideModal.event.tsx
@@ -234,7 +234,7 @@ function SideModalEvent({
     <>
       <motion.div
         className={`bg-white shadow-lg rounded-lg p-4 z-20 absolute right-0 top-0 h-[100%]
-        sm:w-[500px] w-[90%] lg:z-10 lg:static`}
+        sm:w-[400px] w-[85%] lg:z-10 lg:static`}
         initial={{ x: 300, opacity: 0 }}
         animate={{ x: 0, opacity: 1 }}
         exit={{ x: 300, opacity: 0 }}
--- src/components/skeleton/calendar.event.tsx
+++ src/components/skeleton/calendar.event.tsx
@@ -2,7 +2,7 @@ import { Skeleton } from "../ui/skeleton";
 
 export default function CalendarEventSkeleton() {
   return (
-    <div className="bg-white shadow-lg rounded-lg p-4 w-full z-20 absolute right-0 top-0 h-[100%] md:w-[500px] lg:z-10 lg:static">
+    <div className="bg-white shadow-lg rounded-lg p-4 z-20 absolute right-0 top-0 h-[100%] md:w-[400px] w-[85%] lg:z-10 lg:static">
       <div className="w-full mb-[20px]">
         <Skeleton className="h-[24px] w-1/2 mb-4" />
         <div className="mb-[20px]">
