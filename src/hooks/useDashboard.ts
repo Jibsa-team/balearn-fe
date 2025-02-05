90745b21ed03c01f9dd84c25385a0b0b53cd2480
JaeIn1 <97165077+JaeIn1@users.noreply.github.com>
2025-02-05 15:16:50 +0900

    feat : 대시보드 , 주간 일정 데이터 커스텀 훅으로 분리

---
+++ src/hooks/useDashboard.ts
@@ -0,0 +1,18 @@
+import { useQuery } from "@tanstack/react-query";
+import { DashboardType } from "@/types/dashboard/dashboard";
+import { getDashBoard } from "@/app/api/dashboard";
+
+export function useNotify(id: string) {
+  const fetchNotifyData = async (): Promise<DashboardType> => {
+    const response = await getDashBoard(id);
+    if (!response.ok) {
+      throw new Error("공지를 불러오지 못했습니다.");
+    }
+    return response.json();
+  };
+
+  return useQuery({
+    queryKey: ["dashboardData", id] as const,
+    queryFn: fetchNotifyData,
+  });
+}
