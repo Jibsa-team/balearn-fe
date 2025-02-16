eca0777e00218f678d02fb091154a814ef1f17af
JaeIn1 <97165077+JaeIn1@users.noreply.github.com>
2025-02-16 14:29:16 +0900

    refactor  : 채팅 , 사이드바 높이 수정

--- src/app/(with-header)/chat/[id]/page.tsx
+++ src/app/(with-header)/chat/[id]/page.tsx
@@ -4,16 +4,20 @@ import React, { useRef, useState } from "react";
 import { Client } from "@stomp/stompjs";
 import ChatMessageList from "./ChatMessageList";
 import ChatMessageInput from "./ChatMessageInput";
+import { useViewport } from "@/hooks/useViewport";
 
 function ChatPage() {
   const [isConnected, setIsConnected] = useState(false);
   const [connectionError, setConnectionError] = useState<string | null>(null);
   const clientRef = useRef<Client | null>(null);
+  useViewport();
 
   return (
     <div
       className="w-full flex flex-col bg-gray-50"
-      style={{ height: "calc(100vh - 70px)" }}
+      style={{
+        height: "calc(var(--vh, 1vh) * 100 - 70px)",
+      }}
     >
       {connectionError && (
         <div className="bg-red-100 text-red-700 p-2 text-center">
